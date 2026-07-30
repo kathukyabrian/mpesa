f3e891438af15919f18bba05a2251b527e11f668
kathukyabrian <briankathukya2000@gmail.com>
2026-07-31 00:57:59 +0300

    removed unused passkey variable in external query trx request

--- src/main/java/io/github/kathukyabrian/dto/ExternalQueryTransactionRequest.java
+++ src/main/java/io/github/kathukyabrian/dto/ExternalQueryTransactionRequest.java
@@ -6,7 +6,6 @@ import lombok.Data;
 public class ExternalQueryTransactionRequest {
     private String originatorConversationId;
     private String shortCode;
-    private String passkey;
     private String consumerSecret;
     private String consumerKey;
     private String passKey;
